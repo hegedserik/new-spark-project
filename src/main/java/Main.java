aaceb46019500993e489ed4fde693dc1cf197aaa
Erik Hegedus <Erik_Hegedus@epam.com>
2016-07-18 16:33:13 +0200

    update rectangle end point

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,6 @@ import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
-
         get("/hello/:name", (req,res) -> {
             String name = req.params("name");
             return "Hello " + name;
@@ -18,7 +17,8 @@ public class Main {
         get("/rectangle", (req, res) -> {
             int x=Integer.parseInt(req.queryParams("x"));
             int y=Integer.parseInt(req.queryParams("y"));
-            return "Kerület: " + 2*(x+y) + "  "+ "Terület: " + (x*y);
+            return "<html><head></head><body><table><tr><th>Perimeter</th><th>Area</th></tr><tr>" +
+                    "<td>"+2*(x+y)+"</td><td>"+(x*y)+"</td></tr></table></body></html>";
         });
     }
 }
